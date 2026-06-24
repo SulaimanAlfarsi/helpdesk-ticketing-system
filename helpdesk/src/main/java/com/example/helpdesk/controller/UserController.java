9d519f5f14b65b8aba319f902e83148c0cea5a11
Mariya <lamki01@hotmail.com>
2026-06-24 22:29:12 +0400

    UserController.java modified

--- helpdesk/src/main/java/com/example/helpdesk/controller/UserController.java
+++ helpdesk/src/main/java/com/example/helpdesk/controller/UserController.java
@@ -6,10 +6,9 @@ import com.example.helpdesk.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,5 +23,9 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody CreateUserRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(request));
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 }
